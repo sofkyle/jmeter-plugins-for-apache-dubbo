528850cf8e3120fb14efb890d4ad0d143dd96a4d
KyleChou <fightzyf1992@hotmail.com>
2019-10-03 14:06:46 +0800

    fix encoding error

--- src/test/java/io/github/ningyu/jmeter/plugin/test.java
+++ src/test/java/io/github/ningyu/jmeter/plugin/test.java
@@ -8,7 +8,9 @@ import io.github.ningyu.jmeter.plugin.dubbo.sample.MethodArgument;
 import io.github.ningyu.jmeter.plugin.util.ClassUtils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <功能描述>
@@ -22,12 +24,38 @@ public class test {
     }
 
     private static void testEnumB() {
-        List<String> paramterTypeList = new ArrayList<>();
-        List<Object> parameterValuesList = new ArrayList<>();
-        MethodArgument arg = new MethodArgument("io.github.ningyu.jmeter.plugin.EnumB1", "PASSED");
-        ClassUtils.parseParameter(paramterTypeList, parameterValuesList, arg);
-        System.out.println(paramterTypeList.toString());
-        System.out.println(parameterValuesList.toString());
+        HashMap<OwnString,Integer> map = new HashMap<>(15);
+        map.put(new OwnString(), 1);
+         map.put(new OwnString(), 2);
+        map.put(new OwnString(), 3);
+    }
+
+    public static class OwnString implements CharSequence {
+
+        @Override
+        public int length() {
+            return 0;
+        }
+
+        @Override
+        public char charAt(int index) {
+            return 0;
+        }
+
+        @Override
+        public CharSequence subSequence(int start, int end) {
+            return null;
+        }
+
+        @Override
+        public String toString() {
+            return "1".intern();
+        }
+
+        @Override
+        public int hashCode() {
+            return 1;
+        }
     }
 
     private static void testEnumA() {
